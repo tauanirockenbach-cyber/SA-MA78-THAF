08d92e69159c106f6a91626c0e09f47d648e6aa4
Henrique Miguel Deschamps Dahmer <henriquedahmer1234@gmail.com>
2026-07-01 16:46:10 -0300

    Atualizei os usuários e atribuição de roles
    Também testei a role: 'role_admin_manutenção' e estava rodando 100%

--- Scripts/roles.sql
+++ Scripts/roles.sql
@@ -1,19 +1,15 @@
--- Active: 1782931087954@@manutencao-thaf-samanutencao.b.aivencloud.com@16536@Manutencao
+-- Criar os utilizadores corretamente com '%' (permitindo o acesso a partir do seu IP)
+CREATE USER 'tauani'@'%' IDENTIFIED BY 'tauani123';
+CREATE USER 'felipe'@'%' IDENTIFIED BY 'felipe123';
+CREATE USER 'ana'@'%' IDENTIFIED BY 'ana123';
+CREATE USER 'henrique'@'%' IDENTIFIED BY 'henrique123';
 
-#Criação dos usuários
-CREATE USER 'tauani'@'localhost' IDENTIFIED BY'tauani123';
-CREATE USER 'felipe'@'localhost' IDENTIFIED BY'felipe123';
-CREATE USER 'ana'@'localhost' IDENTIFIED BY'ana123';
-CREATE USER 'henrique'@'localhost' IDENTIFIED BY'henrique123';
-
-
-#Criação das Roles
+-- Criação das Roles
 CREATE ROLE 'role_admin_manutencao',
 'role_supervisor_manutencao', 
 'role_tecnico_manutencao', 
 'role_auditor_manutencao';
 
--- 3. Atribuição de Privilégios para as Roles
 -- Privilégio total
 GRANT ALL PRIVILEGES ON Manutencao.* TO 'role_admin_manutencao';
 
@@ -26,21 +22,17 @@ GRANT SELECT, INSERT ON Manutencao.* TO 'role_tecnico_manutencao';
 -- Privilégio apenas de leitura
 GRANT SELECT ON Manutencao.* TO 'role_auditor_manutencao';
 
--- 4. Associação das Roles aos Usuários (Exemplo de distribuição)
-GRANT 'role_admin_manutencao' TO 'tauani'@'localhost';
+GRANT 'role_admin_manutencao' TO 'tauani'@'%';
+GRANT 'role_supervisor_manutencao' TO 'felipe'@'%';
+GRANT 'role_tecnico_manutencao' TO 'ana'@'%';
+GRANT 'role_auditor_manutencao' TO 'henrique'@'%';
 
-GRANT 'role_supervisor_manutencao' TO 'felipe'@'localhost';
+SET DEFAULT ROLE 'role_admin_manutencao' TO 'tauani'@'%';
+SET DEFAULT ROLE 'role_supervisor_manutencao' TO 'felipe'@'%';
+SET DEFAULT ROLE 'role_tecnico_manutencao' TO 'ana'@'%';
+SET DEFAULT ROLE 'role_auditor_manutencao' TO 'henrique'@'%';
 
-GRANT 'role_tecnico_manutencao' TO 'ana'@'localhost';
-
-GRANT 'role_auditor_manutencao' TO 'henrique'@'localhost';
-
-
--- 5. Ativação das Roles (Para que as roles sejam ativas quando o usuário logar)
-SET DEFAULT ROLE 'role_admin_manutencao' TO 'tauani'@'localhost';
-SET DEFAULT ROLE 'role_supervisor_manutencao' TO 'felipe'@'localhost';
-SET DEFAULT ROLE 'role_tecnico_manutencao' TO 'ana'@'localhost';
-SET DEFAULT ROLE 'role_auditor_manutencao' TO 'henrique'@'localhost';
-
--- 6. Aplicação das mudanças
+-- Aplicar todas as alterações
 FLUSH PRIVILEGES;
+
+-- SELECT * FROM mysql.user;
